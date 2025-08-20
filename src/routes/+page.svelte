67f8094bfbb0b92cce783e64ac1367363d64c971
Jeremiah Saunders <46662314+UCYT5040@users.noreply.github.com>
2025-08-20 22:52:04 +0000

    Add floating peg while dragging pegs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -43,11 +43,11 @@
 	let action: null | 'copy' | 'move' = $state(null);
 	let origin: null | number = $state(null); // -1 for library
 	let librarySelection: null | PegConfig = $state(null);
-
-	$inspect(librarySelection);
+	let selection: null | number = $state(null); // Represents the peghole under the mouse
+	let mousePosition: [number, number] = $state([0, 0]); // Actual mouse coordinates
 
 	function mouseDown(position: number, e: UIEvent) {
-		console.log('Mouse down')
+		console.log('Mouse down');
 		e = e as MouseEvent; // Probably a better way to do this (should be handled in PegHole???)
 		e.preventDefault();
 		origin = position;
@@ -63,7 +63,7 @@
 	}
 
 	function mouseUp(position: number, e: UIEvent) {
-		console.log('Mouse up')
+		console.log('Mouse up');
 		if (origin === null) return;
 		let source: PegConfig;
 		if (origin === -1) {
@@ -77,19 +77,35 @@
 		if (action == 'move') {
 			pegHoles[position] = source;
 			pegHoles[origin] = null;
+			pegHoles = pegHoles;
 		} else if (action == 'copy') {
-			pegHoles[position] = { ...source } as PegConfig;
+			pegHoles[position] = source;
+			pegHoles = pegHoles;
 		}
+		action = null;
+	}
 
+	function mouseoverFocus(position: number, e: UIEvent) {
+		selection = position;
+	}
+
+	function mouseoutBlur(position: number, e: UIEvent) {
+		selection = null;
 	}
 
 	function setLibrarySelection(selection: PegConfig) {
-		console.log('Set lib select')
 		librarySelection = selection;
 		action = 'copy';
 		origin = -1;
 	}
+
+	function mousemove(e: MouseEvent) {
+		mousePosition[0] = e.clientX;
+		mousePosition[1] = e.clientY;
+	}
 </script>
+
+<svelte:window onmousemove={mousemove} />
 
 <div class="flex h-screen w-full">
 	<div>
@@ -97,7 +113,14 @@
 		{#each pegLibrary as row (row)}
 			<div class="flex gap-1">
 				{#each row as peg (peg)}
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" class="h-12 w-12" role="button" tabindex="0" onmousedown={() => setLibrarySelection(peg)}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 100 100"
+						class="h-12 w-12"
+						role="button"
+						tabindex="0"
+						onmousedown={() => setLibrarySelection(peg)}
+					>
 						<Peg {peg} />
 					</svg>
 				{/each}
@@ -110,7 +133,28 @@
 		class="m-1 grid flex-1 grid-cols-[repeat(auto-fill,minmax(3rem,1fr))] justify-center gap-1"
 	>
 		{#each pegHoles as peg, index (index)}
-			<PegHole {peg} position={index} onmousedown={mouseDown} onmouseup={mouseUp} />
+			<PegHole
+				{peg}
+				position={index}
+				onmousedown={mouseDown}
+				onmouseup={mouseUp}
+				onmouseoverFocus={mouseoverFocus}
+				onmouseoutBlur={mouseoutBlur}
+			/>
 		{/each}
 	</div>
 </div>
+
+{#if action}
+    <!-- Possibly move this to the script? -->
+	{@const source = origin ? (origin === -1 ? librarySelection : pegHoles[origin]) : null}
+	{#if source}
+		<svg xmlns="http://www.w3.org/2000/svg"
+			class="pointer-events-none absolute w-12 h-12"
+			style="left: {mousePosition[0]}px; top: {mousePosition[1]}px;"
+			viewBox="0 0 100 100"
+		>
+			<Peg peg={source} />
+		</svg>
+	{/if}
+{/if}
